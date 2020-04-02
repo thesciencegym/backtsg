9ff80d5d021002b7032c48d97faf1f777f81876d
zeinab <zeinabelshorbagy@gmail.com>
2020-04-02 14:49:41 +0200

    callback functions integration

--- app/controllers/accept_payment_controller.rb
+++ app/controllers/accept_payment_controller.rb
@@ -1,15 +1,30 @@
 class AcceptPaymentController < ApplicationController
-  def notification_callback
-    p '**************'
-    p "in notification_callback params is #{params}"
-    p '***************'
-    render json: "ok", status: :ok
-  end
+  before_action :set_txn_codes, only: :transaction_response
 
   def transaction_callback
-    p '**************'
-    p "in transaction_callback params is #{params}"
-    p '***************'
-    render json: "ok", status: :ok
+    request.post? ? transaction_proceed : transaction_response
+  end
+
+  def transaction_proceed
+    success = params['obj']['success']
+    render json: { "transaction status": success }, status: :ok
+  end
+
+  def transaction_response
+    txn_msg = @txn[params['txn_response_code']]
+    render json: { "payment_response": txn_msg }, status: :ok
+  end
+
+  private
+
+  def set_txn_codes
+    @txn = {
+      "0": 'success',
+      "1": 'There was an error processing the transaction',
+      "2": 'Contact card issuing bank',
+      "4": 'Expired Card',
+      "5": 'Insufficient Funds',
+      "6": 'Payment is already being processed'
+    }
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   post 'bing', to: 'users#bing'
+  get 'bing', to: 'users#bing'
   post 'payment_webhook', to: 'accept_payment#transaction_callback'
   get 'payment_webhook', to: 'accept_payment#transaction_callback'
-  post 'paymob_notification_callback', to: 'accept_payment#notification_callback'
-  get 'paymob_txn_response_callback', to: 'accept_payment#notification_callback'
 end
