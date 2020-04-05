d41d33d69da46f3909a62ab07b17ed5a7cb1a540
zeinab <zeinabelshorbagy@gmail.com>
2020-04-05 14:35:11 +0200

    removing get callback

--- app/controllers/accept_payment_controller.rb
+++ app/controllers/accept_payment_controller.rb
@@ -2,10 +2,6 @@ class AcceptPaymentController < ApplicationController
   before_action :set_txn_codes
 
   def transaction_callback
-    request.post? ? transaction_proceed : transaction_response
-  end
-
-  def transaction_proceed
     success = params['obj']['success']
     if success
       order = Order.find_by(accept_order_id: params['obj']['order']['id'])
@@ -13,30 +9,5 @@ class AcceptPaymentController < ApplicationController
       order.save!
     end
     render json: { "transaction status": success }, status: :ok
-  end
-
-  def transaction_response
-    result = @txn[params['txn_response_code']]
-    # replace this with wordpresslink
-    url = 'https://www.google.com'
-    if params['txn_response_code'] == '0'
-      redirect_to url, :flash => { :success => 'payment done successfully' }
-    else
-      redirect_to url, :flash => { :error => result }
-    end
-    # render json: { "result": result, "txn_response_code": params['txn_response_code']}, status: :ok
-  end
-
-  private
-
-  def set_txn_codes
-    @txn = {
-      '0' => 'success',
-      '1' => 'There was an error processing the transaction',
-      '2' => 'Contact card issuing bank',
-      '4' => 'Expired Card',
-      '5' => 'Insufficient Funds',
-      '6' => 'Payment is already being processed'
-    }
   end
 end
