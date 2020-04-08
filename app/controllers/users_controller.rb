37cd2171d5612efede044936b62d0e6435b1b1cf
Heba Khazbak <heba.khazbak@gmail.com>
2020-04-08 14:01:18 +0200

    Fix amount to parse it as string

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,7 +24,8 @@ class UsersController < ApplicationController
 
     order_record.payment_token = p_token['token']
     order_record.save!
-    url = "https://accept.paymobsolutions.com/api/acceptance/iframes/#{ENV['ACCEPT_IFRAME_ID']}?payment_token=" + p_token['token'] + "&amount=" + (@product.price/100)
+    amount = (@product.price/100).to_i
+    url = "https://accept.paymobsolutions.com/api/acceptance/iframes/#{ENV['ACCEPT_IFRAME_ID']}?payment_token=" + p_token['token'] + "&amount=" + amount.to_s
     redirect_to url
     # render json: p_token, status: :ok
   end
