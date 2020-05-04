36b9d76747619b4cca1d564c05b9fdd9c3a28876
Heba Khazbak <>
2020-05-04 12:52:58 +0200

    Add rack-cors

--- config/application.rb
+++ config/application.rb
@@ -24,6 +24,15 @@ module TsgBackend
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins 'http://localhost:3000', 'https://tsg2go.thesciencegym.net'
+        resource '*',
+          :headers => :any,
+          :methods => [:get, :post, :options, :delete, :put, :head]
+      end
+    end
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
