9616e6fe4fe28f8375a3ffdde7ae533e53ab3068
zeinab <zeinabelshorbagy@gmail.com>
2020-05-20 14:00:08 +0200

    typo

--- app/services/order_service.rb
+++ app/services/order_service.rb
@@ -1,5 +1,5 @@
 class OrderService
-  def self.create(user_id, product, status, special_price = nil, payment_meyhod)
+  def self.create(user_id, product, status, special_price = nil, payment_method)
     order = Order.new(user_id: user_id, status: status, product_id: product.id,
                       payment_method: payment_method)
     order.price = special_price ? product.special_price : product.price
