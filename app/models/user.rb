dd5e609ae300502db74d210d9c8f9fdd57d92930
zeinab <zeinabelshorbagy@gmail.com>
2020-05-20 14:12:14 +0200

    user

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,18 +12,16 @@ class User < ApplicationRecord
     response = get(@url, sync)
     @users = response['result']
     @users.each do |user|
-      unless User.exists?(email: user['email'])
+      if !User.exists?(email: user['email'])
         new_user = User.create!(first_name: user['firstname'], last_name: user['lastname'],
                             email: user['email'], member_id: user['member_id'],
                             mobile: user['mobile'], vg_user_id: user['user_id'],
                             gender: user['gender'], city: user['place'],
                             timestamp_edit: user['timestamp_edit'])
         p "user #{new_user.email} is found or created"
-      end
-
-      if user_in_db = User.find_by(email: user['email']) && !user_in_db.member_id
-        user_in_db.update(member_id: user['member_id'], vg_user_id: user['user_id'],
-                          timestamp_edit: user['timestamp_edit'])
+      else
+        exis_user = User.find_by(email: user['email'])
+        exis_user.update(member_id: user['member_id'], vg_user_id: user['user_id'], timestamp_edit: user['timestamp_edit'])  unless exis_user.member_id
       end
     end
   end
